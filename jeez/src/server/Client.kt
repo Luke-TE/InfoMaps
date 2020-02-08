8bcbe11f63b918f8d847e68a70384eca24b51e2f
DylanHillier <dylanslavinhillier@gmail.com>
2020-02-08 23:03:58 +0000

    Edited docker file and application.conf so we can run the servers on the same machine

--- jeez/src/server/Client.kt
+++ jeez/src/server/Client.kt
@@ -7,9 +7,7 @@ import io.ktor.client.request.url
 class Client(val ktorClient: HttpClient) {
     suspend fun postToWiki(countries: List<String>, searchTerm: String): String {
         return ktorClient.post<String>() {
-            url("https://5d539037.ngrok.io")
-//            headers.append("countries",countries.toString())
-//            headers.append("query",searchTerm)
+            url("https://6579726e.ngrok.io")
             body = jacksonObjectMapper().writeValueAsString(WikiOutputJsonClass(searchTerm, countries))
         }
     }
