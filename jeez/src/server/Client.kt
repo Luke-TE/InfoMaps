112355d953a17550957bda0838ac94d31c61c48e
DylanHillier <dylanslavinhillier@gmail.com>
2020-02-09 06:14:43 +0000

    No idea

--- jeez/src/server/Client.kt
+++ jeez/src/server/Client.kt
@@ -7,7 +7,7 @@ import io.ktor.client.request.url
 class Client(val ktorClient: HttpClient) {
     suspend fun postToWiki(countries: List<String>, searchTerm: String): String {
         return ktorClient.post<String>() {
-            url("https://d63e30ff.ngrok.io")
+            url("https://5d78683f.ngrok.io")
             body = jacksonObjectMapper().writeValueAsString(WikiOutputJsonClass(searchTerm, countries))
         }
     }
