c2f403491a04c10424bb84dde755d779622c5738
David Valaczaki <dmv18@ic.ac.uk>
2020-02-09 00:33:51 +0000

    hi

--- jeez/src/server/Parser.java
+++ jeez/src/server/Parser.java
@@ -3,6 +3,7 @@ package server;
 import edu.stanford.nlp.ie.machinereading.structure.Span;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.simple.Sentence;
+import kotlinx.html.P;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -15,7 +16,7 @@ public class Parser {
     private String sentence;
     String host = "192.0.0.1";
     String port = "2323";
-    String mystring = "hello world";
+    String mystring = "trump is the president of the united states.";
 
 
     public Parser() {
@@ -25,13 +26,17 @@ public class Parser {
     public String parse(String inputLine) {
         Sentence sentence = new Sentence(inputLine);
         Properties props = new Properties();
-//        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
+        props.setProperty("outputFormat", "conll");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
         Annotation annotation = new Annotation(mystring);
+        Properties trueCaseProps = new Properties();
+        trueCaseProps.setProperty("truecase.overwriteText", "true");
         pipeline.addAnnotator(new TokenizerAnnotator());
         pipeline.addAnnotator(new WordsToSentencesAnnotator());
+        pipeline.addAnnotator(new TrueCaseAnnotator(trueCaseProps));
         pipeline.annotate(annotation);
-        System.out.println(annotation.toString());
+        pipeline.prettyPrint(annotation, System.out);
+
 
         return "hi";
     }
